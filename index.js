0b0f9fc5841864776422952762ee4e35142c9e1d
Juan <juanfelipecardenas4@gmail.com>
2023-09-28 20:56:56 -0400

    Updated config

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(express.static("static"));
 app.use(express.json());
 app.use("/", auth);
 app.use("/", logger);
-app.use("/api", rateLimit);
+// app.use("/api", rateLimit);
 
 //Routes
 app.use("/register", register);
